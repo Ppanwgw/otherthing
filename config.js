af266eb75a21efc25407107bf2fc759cdabff6e9
Ppanwgw <144905335+Ppanwgw@users.noreply.github.com>
2023-09-13 13:55:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['201206178781', 'ɢᴏᴊᴏ', true],
-  ['967780763155', 'BOT', true], 
+  ['966557787001', 'ɢᴏᴊᴏ', true],
+  ['14084264469', 'BOT', true], 
   [''] 
 ] //Number of owners
 
